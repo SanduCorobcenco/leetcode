42a14a1b0f12fe112dd59b464332b942e0b6a4b6
Sandu Corobcenco <corobcenco.sandu@gmail.com>
2022-04-28 14:41:22 -0700

    adding max Area solved question

---
+++ maxArea/maxArea.js
@@ -0,0 +1,19 @@
+var maxArea = function(height) {
+    let maxWater = 0;
+    let i = 0;
+    let j = height.length - 1;
+    while(i < j) {
+        maxWater = Math.max(maxWater, Math.min(height[i], height[j]) * (j - i))
+        if(i < j) {
+            i++;
+        } else {
+            j--;
+        }
+    }
+    return maxWater;
+    
+};
+
+
+// Time Complexity: O(N);
+// Space Complexity: O(1);
